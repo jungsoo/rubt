bf57c6d5c494279d2f2643f4280ebd5cb6dad12f
Jungsoo Park <jungsoopark96@gmail.com>
2016-10-12 23:07:53 -0400

    Established first connection with tracker
    Refactored the section of code that builds the query string from the
    torrent metadata into its own method.
    
    Right now all it does is prints out the response from the tracker.

--- src/client/RUBTClient.java
+++ src/client/RUBTClient.java
@@ -9,11 +9,18 @@ package client;
 
 import java.io.*;
 import java.util.*;
-import java.nio.charset.Charset;
+import java.net.*;
+
+import java.nio.charset.StandardCharsets;
 
 import GivenTools.*;
 
 public class RUBTClient {
+
+    // Unique, arbitrary peer id
+    private static final byte[] PEER_ID =
+        { 'j', 'u', 'n', 'g', 's', 'o', 'o', 'p', 'a', 'r', 
+          'k', 'j', 'a', 'm', 'i', 'e', 'l', 'i', 'a', 'o' };
 
     private static TorrentInfo getTorrentInfo(String fileName) throws IOException {
 
@@ -44,19 +51,84 @@ public class RUBTClient {
         return data;
     }
 
+    private static String toHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * 3);
+        char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+        for (int i = 0; i < bytes.length; i++) {
+            byte b = bytes[i];
+            byte hi = (byte) ((b >> 4) & 0x0f);
+            byte lo = (byte) (b & 0x0f);
+            sb.append('%').append(hex[hi]).append(hex[lo]);
+        }
+
+        return sb.toString();
+    }
+
+    private static String getQueryString(TorrentInfo info) {
+
+        String infoHash = toHexString(info.info_hash.array());
+        String peerId = toHexString(PEER_ID);
+        String ip = info.announce_url.getHost().toString();
+        String port = String.valueOf(info.announce_url.getPort());
+        String event = "started";
+        String uploaded = "" + 0;
+        String downloaded = "" + 0;
+        String left = String.valueOf(info.file_length);
+        String noPeerId = "1";
+        String compact = "0";
+
+        String qs = "?info_hash=" + infoHash
+                    + "&peer_id=" + peerId
+                    + "&downloaded=" + downloaded
+                    + "&uploaded=" + uploaded
+                    + "&left=" + left
+                    + "&event=" + event;
+
+        return qs;
+    }
+
     public static void main(String[] args) {
 
         if (args.length != 1) {
             System.err.println("ERROR: Enter the location to a .torrent file.");
-            System.exit(-1);
+            System.exit(1);
         }
 
+        TorrentInfo metaInfo = null;
         try {
-            TorrentInfo torrentInfo = getTorrentInfo(args[0]);
-            System.out.println(torrentInfo);
-
+            metaInfo = getTorrentInfo(args[0]);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        /* Build query string */
+        String host = metaInfo.announce_url.toString();
+        String qs = getQueryString(metaInfo);
+
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL(host + qs).openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+
+            System.out.println(con.getResponseCode() + " " + con.getResponseMessage() + '\n');
+
+            InputStream in = con.getInputStream();
+
+            byte[] responseBytes = new byte[in.available()];
+            in.read(responseBytes);
+
+            try {
+                ToolKit.print(Bencoder2.decode(responseBytes));
+            } catch (BencodingException e) {
+                System.err.println("ERROR: Could not decode tracker response.");
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            System.err.println("ERROR: Connection failed.");
+            e.printStackTrace();
+        }
+
     }
 }
